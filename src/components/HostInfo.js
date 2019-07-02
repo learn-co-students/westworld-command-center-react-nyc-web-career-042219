ccc44411f0ad6c9d58d1cab4ebaf9bb1a590762c
Kirbyasdf <kirbyAugustus@gmail.com>
2019-07-01 23:20:52 -0400

    Done.

--- src/components/HostInfo.js
+++ src/components/HostInfo.js
@@ -1,8 +1,16 @@
-import '../stylesheets/HostInfo.css'
-import React, { Component } from 'react'
-import { Radio, Icon, Card, Grid, Image, Dropdown, Divider } from 'semantic-ui-react'
+import "../stylesheets/HostInfo.css";
+import React, { Component } from "react";
+import {
+  Radio,
+  Icon,
+  Card,
+  Grid,
+  Image,
+  Dropdown,
+  Divider
+} from "semantic-ui-react";
 
-export function updateHost(key, value, id, func){
+export function updateHost(key, value, id, func) {
   return fetch(`http://localhost:4000/hosts/${id}`, {
     method: "PATCH",
     headers: {
@@ -12,43 +20,51 @@ export function updateHost(key, value, id, func){
     body: JSON.stringify({
       [key]: value
     })
-  }).then(r=>r.json())
-    .then(change=>{
-      func ?
-      func(change[key], id)
-      :
-      null
   })
-};
+    .then(r => r.json())
+    .then(change => {
+      func ? func(change[key], id) : null;
+    });
+}
 
 export default class HostInfo extends Component {
-  toggle = (e) => {
-      let value = !this.props.selectedHost.active
-      updateHost("active", value, this.props.selectedHost.id, this.props.setActive)
-      console.log("The radio button fired");
-  }
+  toggle = e => {
+    let value = !this.props.selectedHost.active;
+    updateHost(
+      "active",
+      value,
+      this.props.selectedHost.id,
+      this.props.setActive
+    );
+    console.log("The radio button fired");
+  };
 
-  handleChange = (e, {value}) => {
-      updateHost("area", value, this.props.selectedHost.id, this.props.setArea)
-  }
+  handleChange = (e, { value }) => {
+    updateHost("area", value, this.props.selectedHost.id, this.props.setArea);
+  };
 
-  areaOptions = () => this.props.areas.map(area => this.createOptionsHash(area))
-    formatName = (name) => name.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    createOptionsHash = (area) => {
-      return {
-        key: this.formatName(area.name),
-        text: this.formatName(area.name),
-        value: area.name
-      }
-    }
-  render(){
+  areaOptions = () =>
+    this.props.areas.map(area => this.createOptionsHash(area));
+  formatName = name =>
+    name
+      .split("_")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  createOptionsHash = area => {
+    return {
+      key: this.formatName(area.name),
+      text: this.formatName(area.name),
+      value: area.name
+    };
+  };
+  render() {
     return (
       <Grid>
         <Grid.Column width={6}>
           <Image
             src={this.props.selectedHost.imageUrl}
-            floated='left'
-            size='small'
+            floated="left"
+            size="small"
             className="hostImg"
           />
         </Grid.Column>
@@ -56,18 +72,30 @@ export default class HostInfo extends Component {
           <Card>
             <Card.Content>
               <Card.Header>
-                {this.props.selectedHost.firstName}   {this.props.selectedHost.lastName} | {this.props.selectedHost.gender === "Male" ? <Icon name='man' /> : <Icon name='woman' />}
+                {this.props.selectedHost.firstName}{" "}
+                {this.props.selectedHost.lastName} |{" "}
+                {this.props.selectedHost.gender === "Male" ? (
+                  <Icon name="man" />
+                ) : (
+                  <Icon name="woman" />
+                )}
               </Card.Header>
               <Card.Meta>
                 <Radio
                   onChange={this.toggle}
-                  label={this.props.selectedHost.active ?  "Active" : "Decommissioned"}
+                  label={
+                    this.props.selectedHost.active ? "Active" : "Decommissioned"
+                  }
                   checked={this.props.selectedHost.active}
                   slider
                 />
               </Card.Meta>
               <Divider />
-              Current Area: {this.props.selectedHost.area.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+              Current Area:{" "}
+              {this.props.selectedHost.area
+                .split("_")
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
               <Dropdown
                 onChange={this.handleChange}
                 value={this.props.selectedHost.area}
@@ -78,6 +106,6 @@ export default class HostInfo extends Component {
           </Card>
         </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
--- src/components/LogPanel.js
+++ src/components/LogPanel.js
@@ -1,66 +1,68 @@
-import React from 'react'
-import { Segment, Button } from 'semantic-ui-react';
-import { Log } from '../services/Log'
-import { updateHost } from "./HostInfo"
-let logs = []
+import React from "react";
+import { Segment, Button } from "semantic-ui-react";
+import { Log } from "../services/Log";
+import { updateHost } from "./HostInfo";
+let logs = [];
 
-
-export default class  LogPanel extends React.Component {
-
+export default class LogPanel extends React.Component {
   state = {
     allActive: false
-  }
+  };
 
   triggerLogs = (action, status) => {
-    switch(action) {
+    switch (action) {
       case "allActivate": {
-        logs.unshift(Log.warn("ALL ACTIVIATED"))
-        Promise.all(this.props.hosts.map(host=> updateHost("active", status, host.id)))
-        .then(()=>this.props.setAllStatus(status))
-        this.setState({allActive: !this.state.allActive})
+        logs.unshift(Log.warn("ALL ACTIVIATED"));
+        Promise.all(
+          this.props.hosts.map(host => updateHost("active", status, host.id))
+        ).then(() => this.props.setAllStatus(status));
+        this.setState({ allActive: !this.state.allActive });
         break;
       }
       case "allUnactivate": {
-        logs.unshift(Log.warn("ALL DEACTIVATED"))
-        Promise.all(this.props.hosts.map(host=> updateHost("active", status, host.id)))
-        .then(()=>this.props.setAllStatus(status))
-        this.setState({allActive: !this.state.allActive})
-      break;
-    }
+        logs.unshift(Log.warn("ALL DEACTIVATED"));
+        Promise.all(
+          this.props.hosts.map(host => updateHost("active", status, host.id))
+        ).then(() => this.props.setAllStatus(status));
+        this.setState({ allActive: !this.state.allActive });
+        break;
+      }
       case "oneActive":
-        logs.unshift(Log.notify("Host going ACTIVE"))
+        logs.unshift(Log.notify("Host going ACTIVE"));
         break;
 
       case "oneUnactive":
-        logs.unshift(Log.notify("Host DEACTIVATED"))
+        logs.unshift(Log.notify("Host DEACTIVATED"));
         break;
-    default:
-      console.log("LOG: *error* ")
+      default:
+        console.log("LOG: *error* ");
     }
-  return logs
+    return logs;
+  };
+
+  triggerStatusAll = () => {
+    this.state.allActive
+      ? this.triggerLogs("allUnactivate", false)
+      : this.triggerLogs("allActivate", true);
+  };
+
+  render() {
+    return (
+      <Segment className="HQComps" id="logPanel">
+        <pre>
+          {logs.map((log, i) => (
+            <p key={i} className={log.type}>
+              {log.msg}
+            </p>
+          ))}
+        </pre>
+        <Button
+          fluid
+          color={this.state.allActive ? "green" : "red "}
+          content={this.state.allActive ? "DEACTIVATE ALL" : "ACTIVATE ALL"}
+          onClick={() => this.triggerStatusAll()}
+        />
+      </Segment>
+    );
   }
-
-  triggerStatusAll = () =>{
-    this.state.allActive ?
-      this.triggerLogs("allUnactivate", false)
-      :
-     this.triggerLogs("allActivate", true)
-  }
-
-
-  render(){
-  return(
-    <Segment className="HQComps" id="logPanel">
-      <pre>
-        {logs.map((log, i) => <p key={i} className={log.type}>{log.msg}</p>)}
-      </pre>
-      <Button
-        fluid
-        color={this.state.allActive ? "green" : "red "}
-        content={this.state.allActive ? "DEACTIVATE ALL" : "ACTIVATE ALL"}
-        onClick={()=>this.triggerStatusAll()}
-      />
-    </Segment>
-    )
- }
 }
